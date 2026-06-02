e678702eaeaa3a6a8646c2e013971c87c363cc3e
Ariel <ariel.galvao@gmail.com>
2026-06-01 21:52:38 -0300

    Instruções GitHub x conexão com IDE

--- OlaMundo/SuperTrunfo_C.c
+++ OlaMundo/SuperTrunfo_C.c
@@ -1,5 +1,51 @@
 #include <stdio.h>
 
+    // para rodar o código, use o terminal da bash e digite: ./super_trunfo 
+    
+    /* Usabilidade github
+    ---Primeira vez: 
+    abrir git bash, git init (inicia repositório) 
+    git add . (adiciona os arquivos para o commit)
+    git commit -m "Meu primeiro commit/comentário identificador da alteração realizada" (faz o commit)   
+    git branch -M main (cria a branch main, se atentar porque no vs code também precisa estar como main)
+    
+    conectando com GIthub: Criar repositório no github, copiar o link do repositório, voltar para o terminal e digitar: 
+    git remote add origin (link do repositório copiado)
+    git push -u origin main (envia o código para o github ´pela primeira vez, depois disso, para enviar as alterações basta 
+    usar o comando git push origin main)
+
+    --- Rotina de atualização:
+    git add . = adiciona os arquivos para o commit
+    git commit -m "Comentário identificador da alteração realizada" = faz o commit
+    git push origin main = envia as alterações para o github    
+    
+    Importante!!!!
+    sempre pedir "git status" para verificar o status do repositório, quais arquivos foram modificados, 
+    quais estão prontos para o commit, etc.
+
+    sempre se atentar se a pasta que está sendo digitado os comando está correta, para isso usar o comando "pwd" 
+    para verificar o caminho da pasta atual, e "ls" para listar os arquivos da pasta atual.
+    
+    --- Letreiro de comando: 
+
+    Untracked - Arquivo novo; o Git ainda não o conhece. 
+    - use git add para começar a monitorar esse arquivo.
+
+    Modified - Arquivo modificado; o Git já o conhece, mas ele foi alterado desde o último commit. 
+    - use git add para incluir essas alterações no próximo commit.
+
+    Added - Arquivo adicionado; o Git já o conhece e ele foi incluído no próximo commit. 
+    - use git commit para confirmar essas alterações.
+
+    Deleted  - Arquivo deletado; o Git já o conhece, mas ele foi removido desde o último commit. 
+    - use git add para incluir essa alteração no próximo commit.
+
+    Renamed - Arquivo renomeado; o Git já o conhece, mas ele foi renomeado desde o último commit. 
+    - use git add para incluir essa alteração no próximo commit.
+
+
+    */
+
 int main()  {
     // Desafio Super Trunfo - Países
     // Tema 1 - Cadastro das cartas	
@@ -29,7 +75,7 @@ int main()  {
     printf("Digite o Estado (A a H): ");
     scanf(" %c", &estado1);
 
-    printf("Digite o código da carta:  ");
+    printf("Digite o codigo da carta:  ");
     scanf(" %s", codigo1);
 
     printf("Digite o nome da cidade: "); // [^\n] para ler string com espaços
